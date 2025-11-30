4fd083ce97d41cb84449b47f550793548a1a2d34
Nihad <ashrafurr558@gmail.com>
2025-11-30 09:10:02 +0600

    First commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.firmwise {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.firmwise to javafx.fxml;
+    exports com.example.firmwise;
+}
